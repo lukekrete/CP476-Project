0f02903ab294f3081e059e707ee87b43786fb2b1
lukekrete <lukekrete@gmail.com>
2020-03-03 21:53:21 -0500

    Add files via upload

---
+++ final-website/src/login.js
@@ -0,0 +1,11 @@
+function checkLogin(event){
+    var user = document.getElementById('username');
+    var pass = document.getElementById('password');
+
+    if(user != "" & pass != ""){
+        // send api call to check if legal user/pass in database
+    }else{
+        //Add error child to login page.
+    }
+
+}
